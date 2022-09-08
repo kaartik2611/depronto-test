3d1d03ad6d97abe61c8efc2ac0be6cfd3a712ddb
Kaartik Nayak <kaartiknayak2001@gmail.com>
2022-09-08 16:18:16 +0530

    Form done

---
+++ form.js
@@ -0,0 +1,17 @@
+const form = document.getElementById("form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fname = form.name.value;
+  date = form.date.value;
+  ftitle = form.feedback.value;
+  feedback = form.feedbacktext.value;
+  gender = form.gender.value;
+  check = form.checkbox;
+  check_arr = [];
+  check.forEach((item) => {
+    if (item.checked) {
+      check_arr.push(item.value);
+    }
+  });
+  console.log(fname, date, ftitle, feedback, gender, check_arr);
+});
